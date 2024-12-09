954335cbf441afa9770beb5ddee151fa1acc1395
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2024-12-09 22:31:42 +0530

    kdhjfdk

--- mydemopro/urls.py
+++ mydemopro/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 from app1 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('app1.urls')),
-	path('app2/' , include('app2.urls')),
+    # path('' , include('app1.urls')),
+	# path('app2/' , include('app2.urls')),
 ]
